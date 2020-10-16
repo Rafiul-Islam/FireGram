d78e5d3941f19f53594409c1cabc680c1d8c85ab
Rafiul Isalam <[shohel.rana@mymedicalhub.com]>
2020-10-17 04:58:03 +0600

    UI and functionality done

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>FireGram</h1>
+      <h2>Your Pictures</h2>
+      <p>We are here to store your sweet memories</p>
+    </div>
+  )
+}
+
+export default Title;
---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+import * as firebase from "firebase";
+import 'firebase/storage';
+import 'firebase/firestore'
+
+// Your web app's Firebase configuration
+let firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectStorage, projectFirestore, timestamp };
